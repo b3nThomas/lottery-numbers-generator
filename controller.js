1fa12df542e8c560d5c86a77392c3963af6a2ccd
Ben Thomas <b3nadthomas@gmail.com>
2017-02-02 15:34:17 +0000

    Add files via upload
    CSS changes to fit mobile better

--- controller.js
+++ controller.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	$('#lucky-dip').click(function() {
 		var range = $('input[type=radio][name=range]:checked').val();
 		var draw = $('input[type=radio][name=draw]:checked').val();
-		$('#yourNumbers').html(luckyDip(1, range, draw).join(" - "));
+		$('#yourNumbers').html(luckyDip(1, range, draw).join("-"));
 	});
 
 
